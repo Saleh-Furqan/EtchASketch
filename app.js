354133ab68a50383745fef2b91cd702b8ba24b20
Saleh-Furqan <iamsaleh.furqan@gmail.com>
2022-04-28 17:48:04 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+let color = 'black'
+let click = true
+
+function populateBoard(size) {
+    let board = document.querySelector('.board')
+    let squares = board.querySelectorAll('div')
+    squares.forEach(div => {
+        div.remove()
+    })
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    let squareCount = size * size
+    for (let i = 0; i < squareCount; i ++) {
+        let square = document.createElement('div')
+        square.style.backgroundColor = 'white'
+        square.addEventListener('mouseenter', colorSquare)
+        board.insertAdjacentElement('beforeend', square)
+    }
+}
+function changeSize(i) {
+    if (i >= 2 || i <= 100) {
+        populateBoard(i)
+    } else {
+        console.log('error')
+    }
+}
+function colorSquare() {
+    if (click) {
+        if (color == 'random') {
+            this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`
+        } else {
+            this.style.backgroundColor = color
+        }
+    }
+}
+function changeColor(c) {
+    color = c    
+}
+function resetBoard() {
+    let board = document.querySelector('.board')
+    let squares = board.querySelectorAll('div')
+    squares.forEach(div => {
+        div.style.backgroundColor = 'white'
+    })
+}
+document.querySelector('body').addEventListener('click', (e) => {
+    if (e.target.tagName != 'BUTTON') {
+        click = !click
+        if (click) {
+            document.querySelector('.mode').textContent = 'Mode: Coloring...'
+        } else {
+            document.querySelector('.mode').textContent = 'Mode: Not Coloring...'
+        }
+    }
+})
+
+
+populateBoard(16)
